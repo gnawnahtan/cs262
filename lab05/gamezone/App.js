3d608ce27e6fff8db3eb34ae5663f6d63aca1f50
Nathan W <gnawnahtan@gmail.com>
2020-10-03 23:26:31 -0400

    Added modifications for lab05

---
+++ lab05/gamezone/App.js
@@ -0,0 +1,30 @@
+// modified by Nathan Wang for CS-262
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Navigator from "./routes/drawer";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
+}
